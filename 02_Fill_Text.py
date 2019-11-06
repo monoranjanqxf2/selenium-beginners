e27dad4196f6101277bf2b51102bc5ce66c1ee03
monoranjanqxf2 <monoranjan.mandal@qxf2.com>
2019-11-06 16:25:42 +0530

    Learned and edited

--- 02_Fill_Text.py
+++ 02_Fill_Text.py
@@ -20,10 +20,10 @@ import time
 from selenium import webdriver
 
 # Create an instance of Firefox WebDriver
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 
 # The driver.get method will navigate to a page given by the URL
-driver.get("http://qxf2.com/selenium-tutorial-main")
+driver.get("http://www.facebook.com/")
 
 # Check if the title of the page is proper
 if(driver.title=="Qxf2 Services: Selenium training main"):
@@ -32,17 +32,17 @@ else:
     print ("Failure: Qxf2 Tutorial page Title is incorrect")    
 
 # Find the name field using xpath with id
-name = driver.find_element_by_xpath("//input[@id='name']")
+name = driver.find_element_by_xpath("//input[@name='email']")
 # KEY POINT: Send text to an element using send_keys method
-name.send_keys('Avinash')
+name.send_keys('monoranjan.mandal@qxf2.com')
 
 # Find the email field using xpath without id
-email = driver.find_element_by_xpath("//input[@name='email']")
-email.send_keys('avinash@qxf2.com')
+email = driver.find_element_by_xpath("//input[@name='pass']")
+email.send_keys('Password')
 
 # Find the phone no field using id
-phone = driver.find_element_by_id('phone')
-phone.send_keys('9999999999')
+#phone = driver.find_element_by_id('phone')
+#phone.send_keys('9999999999')
 
 # Sleep is one way to wait for things to load
 # Future tutorials cover explicit, implicit and ajax waits
